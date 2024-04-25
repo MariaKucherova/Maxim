77f03ff1fab60af0084a7fe419c8cea9d23a49f6
Maria Kucherova <kucherovamaria123@gmail.com>
2024-04-25 12:51:32 +0500

    Добавила решение задачи 1

---
+++ MaximTasks/Task1/MaximTasks.Task1.Lib/StringUtilities.cs
@@ -0,0 +1,35 @@
+﻿namespace MaximTasks.Task1.Lib
+{
+    /// <summary>
+    /// Функции для работы со строками.
+    /// </summary>
+    public static class StringUtilities
+    {
+        /// <summary>
+        /// Если строка содержит чётное количество символов, то метод разделяет её 
+        /// на две подстроки, каждая подстрока переворачивается и соединятся в одну строку.
+        /// Если строка содержит нечётное количество символов, то метод переворачивает эту
+        /// строку и к ней добавляет изначальную строку.
+        /// </summary>
+        /// <param name="str">Входная строка.</param>
+        /// <returns>Возвращает обработанную строку.</returns>
+        public static string Transform(string str)
+        {
+            if (str.Length % 2 == 0)
+            {
+                var middleStr = str.Length / 2;
+
+                var subStr1 = new string(str.Substring(0, middleStr).Reverse().ToArray());
+                var subStr2 = new string(str.Substring(middleStr).Reverse().ToArray());
+                
+                return subStr1 + subStr2;
+            }
+            else
+            {
+                var reversedStr = new string(str.Reverse().ToArray());  
+
+                return reversedStr + str;
+            }
+        }
+    }
+}
---
+++ MaximTasks/Task1/MaximTasks.Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿using MaximTasks.Task1.Lib;
+
+namespace MaximTasks.Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = StringUtilities.Transform("abcdef");
+            Console.WriteLine(result);
+        }
+    }
+}
