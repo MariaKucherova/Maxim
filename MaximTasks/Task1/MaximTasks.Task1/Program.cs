fc99d799c9f997b4fbc3a927bcab3e7010da6be2
Maria Kucherova <kucherovamaria123@gmail.com>
2024-04-30 18:51:28 +0500

    Добавила решение задачи 5

--- MaximTasks/Task1/MaximTasks.Task1/Program.cs
+++ MaximTasks/Task1/MaximTasks.Task1/Program.cs
@@ -12,17 +12,47 @@ namespace MaximTasks.Task1
             var result = StringUtilities.Transform(input);
             Console.WriteLine(result);
 
-            var chars = StringUtilities.CalculateNumberChar(result);
-            foreach (var c in chars)
+            if (!result.Contains("Были введены не подходящие символы:"))
             {
-                Console.WriteLine($"Символ {c.Key} встречается в строке {c.Value} раз.");
-            }
+                var chars = StringUtilities.CalculateNumberChar(result);
+                foreach (var c in chars)
+                {
+                    Console.WriteLine($"Символ {c.Key} встречается в строке {c.Value} раз.");
+                }
 
-            var substring = StringUtilities.GetLongestSubstringVowel(result);
-            if (substring != string.Empty)
-            {
-                Console.WriteLine($"Самая длинная подстрока начинающаяся и заканчивающаяся" +
-                                  $" на гласную букву из «aeiouy»: {substring}");
+                var substring = StringUtilities.GetLongestSubstringVowel(result);
+                if (substring != string.Empty)
+                {
+                    Console.WriteLine($"Самая длинная подстрока начинающаяся и заканчивающаяся" +
+                                      $" на гласную букву из «aeiouy»: {substring}");
+                }
+
+                while (true)
+                {
+                    if (result == string.Empty)
+                    {
+                        break;
+                    }
+
+                    Console.WriteLine("Выберите алгоритм сортировки: \n" +
+                                      "1 - быстрая сортировка\n" +
+                                      "2 - сортировка деревом");
+                    int.TryParse(Console.ReadLine(), out int sorting);
+
+
+                    if (sorting == 1)
+                    {
+                        var sortedResult = SortingAlgorithms.QuickSort(result.ToCharArray(), 0, result.Length - 1);
+                        Console.WriteLine($"Отсортированная строка - {sortedResult}");
+                        break;
+                    }
+                    else if (sorting == 2)
+                    {
+                        var sortedResult = SortingAlgorithms.TreeSort(result.ToCharArray());
+                        Console.WriteLine($"Отсортированная строка - {sortedResult}");
+                        break;
+                    }
+                }
             }
         }
     }
