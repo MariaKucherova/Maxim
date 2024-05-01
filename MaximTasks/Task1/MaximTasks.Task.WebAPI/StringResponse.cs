4c693489922e243de7c2d6b6adb933776ecd81c3
Maria Kucherova <kucherovamaria123@gmail.com>
2024-05-01 19:37:24 +0500

    Добавила решение задачи 7

---
+++ MaximTasks/Task1/MaximTasks.Task.WebAPI/StringResponse.cs
@@ -0,0 +1,35 @@
+﻿namespace MaximTasks.Task.WebAPI
+{
+    /// <summary>
+    /// Тело ответа.
+    /// </summary>
+    public class StringResponse
+    {
+        /// <summary>
+        /// Обработанная строка.
+        /// </summary>
+        public string? Result { get; set; }
+
+        /// <summary>
+        /// Словарь, содержащий информацию о том, сколько раз входил в обработанную
+        /// строку каждый символ.
+        /// </summary>
+        public Dictionary<char, int>? Chars { get; set; }
+
+        /// <summary>
+        /// Самая длинная подстрока начинающаяся и заканчивающаяся на гласную букву
+        /// из «aeiouy».
+        /// </summary>
+        public string? Substring { get; set; }
+
+        /// <summary>
+        /// Отсортированная обработанная строка.
+        /// </summary>
+        public string? SortedResult { get; set; }
+
+        /// <summary>
+        /// «Урезанная» обработанная строка – обработанная строка без одного символа.
+        /// </summary>
+        public string? CutedResult { get; set; }
+    }
+}
